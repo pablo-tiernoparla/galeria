9aeb8e451873d5ef56c29dff56b5cd4a9706195f
pablo-tiernoparla <pablo.delgadosoto@tiernoparla.es>
2023-02-07 21:54:34 +0100

    Modificada excepcion idYaExiste

--- src/main/java/daw/programacion/Galeria.java
+++ src/main/java/daw/programacion/Galeria.java
@@ -280,10 +280,10 @@ public class Galeria {
 
     public void idYaExiste(int num, Obra[] todo) throws IllegalArgumentException {
         for (int i = 0; i < todo.length; i++) {
-            if (num == todo[i].getId()) {
+            if (todo[i] == null) {
+                continue;
+            } else if (num == todo[i].getId()) {
                 throw new IllegalArgumentException();
-            } else if (todo[i] == null){
-                break;
             }
         }
     }
