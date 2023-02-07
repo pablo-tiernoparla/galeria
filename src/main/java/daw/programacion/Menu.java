c39bb937e5da1c515e41bfc36baf601ac7bfb575
pablo-tiernoparla <pablo.delgadosoto@tiernoparla.es>
2023-02-07 20:55:19 +0100

    metodo para enseñar el menu

--- src/main/java/daw/programacion/Menu.java
+++ src/main/java/daw/programacion/Menu.java
@@ -3,6 +3,16 @@ package daw.programacion;
 public class Menu {
     public static void main(String[] args) {
         
-        
+
+    }
+
+    public void enseñarMenu() {
+        final int[] SELECCION_MENU = {0,1,2,3,4,5,6};
+        final String[] OPCIONES = {"Salir", "Visualizar obras", "Dar de alta una obra", "Modificar una obra", "Visualizar una obra", "Obtener el precio de una obra", "Imprimir etiqueta"};
+
+        for (int i = 0; i < SELECCION_MENU.length; i++) {
+            System.out.println(SELECCION_MENU[i] + "" + OPCIONES[i]);
+        }
+        System.out.println("Escoge una opcion");
     }
 }
